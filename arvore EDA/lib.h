5ec3b130d74c5ad790923dd7a87d439ce8a27a1b
Nelson1Aguiar <132019512+Nelson1Aguiar@users.noreply.github.com>
2023-05-26 02:59:39 -0300

    Update lib.h

--- arvore EDA/lib.h
+++ arvore EDA/lib.h
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 11
+#define fator_carga 0.8
 
 typedef struct produto{
     char nomeProduto[50];
@@ -15,6 +19,57 @@ typedef struct No {
     int altura;
 } No;
 
+struct cartao{
+    long int numero;//numero unico do cartao
+    char nome[60];//nome do titular do cartao
+    int dia,mes,ano;//data de validade do cartao
+    No produto;
+};
+typedef struct cartao Card;
+
+struct usuario{
+    long cpf;
+    char endereco[60];
+    long numeroCartao[10];//cartoes cadastrados com limite de 10 cartoes
+    char nome[60]; //nome do usuario
+    int qtdCartoes;//quantos cartoes o usuario tem cadastrado;
+};
+typedef struct usuario User;
+
+typedef struct{
+	Card cartao;//A chave usada será feita com o número do cartao ou com o cpf, dependendo da tabela
+	User usuario;
+} Telemento;
+
+typedef struct{
+        Telemento* elemento;
+        float n; // qtd de elementos inseridos na estrutura
+        float m; //capacidade da tabela
+} TlistaUsuario; // tabela hash de usuarios
+
+typedef struct{
+        Telemento* elemento;
+        float n; // qtd de elementos inseridos na estrutura
+        float m; //capacidade da tabela
+} TlistaCardCompra; //Tabela hash de cartao e compras
+
+
+int hashing(long chave,int tam);
+TlistaUsuario criarUsuario();
+TlistaCardCompra criarCartao();
+void redimensionarUsuario(TlistaCardCompra *C,TlistaUsuario *L,int tam_novo);
+void redimensionarCartao(TlistaUsuario *L,TlistaCardCompra *C,int tam_novo);
+int tamanho(TlistaUsuario L);
+int elemento(TlistaUsuario L, int pos, Telemento *dado);
+void exibirUsuario(TlistaUsuario L);
+int inserirUsuario(TlistaUsuario *L,TlistaCardCompra *C,Telemento dado,Telemento dadoCard);
+int inserirCartao(TlistaUsuario *L,TlistaCardCompra *C, Telemento dado,Telemento dadoUser);
+int buscarUsuario(TlistaUsuario L,long chave);
+void atrelarCartaoUsuario(TlistaUsuario *L,int pos,Telemento dado);
+void exibirCartoes(TlistaCardCompra C);
+void exibirUsuarioUnico(TlistaUsuario L,int pos);
+void ExibirCartaoUnico(TlistaCardCompra C,int endereco);
+int buscarCartao(TlistaCardCompra C,long chave);
 int getAltura(No* no);
 int max(int a, int b);
 No* novoNo(Produto P);
@@ -22,6 +77,5 @@ No* rotacaoDireita(No* y);
 No* rotacaoEsquerda(No* x);
 int getBalanceamento(No* no);
 No* inserir(No* no, Produto P);
-No* busca(No* raiz, Produto P);
 void imprimir(No* no);
-void imprimirUnicaCompra(No* no);
+void juntarArvoreTabela(TlistaCardCompra *C,int endereco,No raiz);
